7c208fe912c5ed50a58b766dc7feb285a6fd8bb0
Dennis Jackson <dennisdjack1987@gmail.com>
2023-06-26 16:28:30 -0700

    error fix

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,7 @@
 export default function Error(){
     return(
         <div style={{height:'100vh',backgroundColor:'black', display:'flex',alignContent:'center',justifyContent:'center'}}>
-       <p className="text-light">"&lt;!-- SC_OFF --&gt;&lt;div class=\"md\"&gt;&lt;p&gt;&lt;a href=\"https://www.pixiv.net/en/artworks/82407513\"&gt;Pixiv&lt;/a&gt;&lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;",</p>
+       <p className="text-ligh">error</p>
     </div>
     )
 }
